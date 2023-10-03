a377a65657f0b28558f83b50a454307c365eb764
dylandigby87 <dylan.m.c.digby@gmail.com>
2023-10-03 19:05:48 +1100

    removed unused files

--- src/components/Profile.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Profile() {
-    return <div className="md:w-32 p-4 text-center shadow-lg hover:bg-slate-600 text-white">Sign in</div>
-}
--- src/components/navigation/UserMenu.tsx
+++
@@ -1,5 +0,0 @@
-export default function UserMenu() {
-    return (
-        <div className="text-white hover:bg-slate-600 p-4 shadow-lg">User Menu</div>
-    )
-}
