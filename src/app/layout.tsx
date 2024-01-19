58df217dae4ab8ce55ca0516c802846c29e8019d
dylandigby87 <dylan.m.c.digby@gmail.com>
2024-01-19 14:22:05 +1100

    adde vercel speed insights properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default async function RootLayout({ children, }: { children: React.ReactN
             </main>
           </Providers>
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider >
