3259166236b70c1fbdeafa1837bc4787f01da4b4
dylandigby87 <dylan.m.c.digby@gmail.com>
2024-01-19 14:21:06 +1100

    adde vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google';
 import { Providers } from "./providers";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Gamer Buddy',
