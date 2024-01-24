779f4e72243177a09e0838d0db917a7d47be8156
dylandigby87 <dylan.m.c.digby@gmail.com>
2024-01-24 19:46:37 +1100

    layout improvements 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,22 +1,22 @@
 export default function About() {
 
     return (
-        <div className="grid grid-flow-row auto-rows-fr tracking-wide ">
-            <div className="">
+        <div className="flex flex-col h-full tracking-wide shadow-sm">
+            <div className="grow flex flex-col">
                 <h1 className="flex justify-around text-3xl text-blue-700 mt-10 mb-6">About</h1>
-                <p className="flex justify-around p-4 mb-0 italic">This website is a portfolio project. I have noticed international traffic and want to be transparent about the usage of this</p>
-                <hr className="my-10" />
-            </div>
+                <p className="grow flex items-center justify-around p-4 mb-0 italic">This website is a portfolio project. I have noticed international traffic and want to be transparent about the usage of this</p>
 
-            <div className="">
-                <div className="p-10">
+            </div>
+            <hr className="" />
+            <div className="grow flex">
+                <div className=" flex items-center justify-around p-10">
                     <p className="">For now I&apos;m not planning to market, or bring people to this website as I am on a free, and limited database plan. The value to users depends on the amount of users actively interacting with others. That&apos;s not going to happen for quite some time! However you are welcome to use this if you wish. Just note that you are not going to find many players at this early stage.</p>
                 </div>
 
-                <hr className="my-10" />
-            </div>
 
-            <div className="flex flex-col justify-around">
+            </div>
+            <hr className="" />
+            <div className="grow flex flex-col justify-around">
                 <p className="mx-auto">
                     Built on
                     <span className="primary-color-font"> NextJS </span> with
