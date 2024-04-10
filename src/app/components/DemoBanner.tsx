50bd4ba8a292a87f8aa5d49c6e02d9dc0a6c4c87
dylandigby87 <dylan.m.c.digby@gmail.com>
2024-04-10 10:00:33 +1000

    tweaked demo banner

--- src/app/components/DemoBanner.tsx
+++ src/app/components/DemoBanner.tsx
@@ -10,9 +10,10 @@ export default function DemoBanner() {
             You can <span className="primary-color-font dark:secondary-color-font">login
                 as a demonstration user</span> with the
             <span className="primary-color-font dark:secondary-color-font"> username </span>
-            of <span className="primary-color-font dark:secondary-color-font">&quot;demo1&quot;</span>,
-            and <span className="primary-color-font dark:secondary-color-font">password</span> of
-            <span className="primary-color-font dark:secondary-color-font"> &quot;demo1&quot;</span>, to view the intended experience as a user with games, connections, requests, etc!
+            and <span className="primary-color-font dark:secondary-color-font">password</span>
+            , of
+            <span className="primary-color-font dark:secondary-color-font"> &quot;demo1&quot;</span>
+            , to view the intended experience as a user with games, connections, requests, etc!
         </div>
     )
 }
